0db7b935b1b5cbc916f51e267e6185243a72dcbb
Taiwo kazeem <kazeem@kobo360.com>
2025-01-20 12:34:52 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+     <p className='text-3xl text-red-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, a.</p>
+    </>
+  )
+}
+
+export default App
